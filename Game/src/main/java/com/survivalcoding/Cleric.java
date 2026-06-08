2a7167788d34d5fbd87051f77db704d6ad7f17bb
Yungchang Cho <dudckd2411@gmail.com>
2026-06-08 13:27:16 +0900

    연습문제 2

--- Game/src/main/java/com/survivalcoding/Cleric.java
+++ Game/src/main/java/com/survivalcoding/Cleric.java
@@ -5,6 +5,12 @@ public class Cleric {
     int hp = 50;
     int mp = 10;
     final int max_hp = 50;
-    final int max_mp = 50;
+    final int max_mp = 10;
+
+    void selfAid() {
+        mp -= 5;
+        hp = max_hp;
+        System.out.println("셀프 에이드");
+    }
 
 }
