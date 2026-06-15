6a5b66ebcc273cdec84e5805645cd6ce5f98be54
Yungchang Cho <dudckd2411@gmail.com>
2026-06-15 17:06:31 +0900

    연습문제 1-2

--- Game/src/main/java/com/survivalcoding/Wand.java
+++ Game/src/main/java/com/survivalcoding/Wand.java
@@ -9,6 +9,9 @@ public class Wand {
     }
 
     public void setName(String name) {
+        if (name == null || name.length() < 3) {
+            throw new IllegalArgumentException("지팡이 이름은 null일 수 없고, 3문자 이상이어야 한다.");
+        }
         this.name = name;
     }
 
@@ -17,6 +20,9 @@ public class Wand {
     }
 
     public void setPower(double power) {
+        if (power < 0.5 || power >= 100.0) {
+            throw new IllegalArgumentException("지팡이의 마력은 0.5이상 100.0 이하여야 한다.");
+        }
         this.power = power;
     }
 }
--- Game/src/main/java/com/survivalcoding/Wizard.java
+++ Game/src/main/java/com/survivalcoding/Wizard.java
@@ -11,6 +11,9 @@ public class Wizard {
     }
 
     public void setName(String name) {
+        if (name == null || name.length() < 3) {
+            throw new IllegalArgumentException("마법사의 이름은 null일 수 없고, 3문자 이상이어야 한다.");
+        }
         this.name = name;
     }
 
@@ -19,6 +22,9 @@ public class Wizard {
     }
 
     public void setHp(int hp) {
+        if (hp < 0) {
+            this.hp = 0;
+        }
         this.hp = hp;
     }
 
@@ -27,6 +33,9 @@ public class Wizard {
     }
 
     public void setMp(int mp) {
+        if (mp < 0) {
+            throw new IllegalArgumentException("MP는 0 이상이어야 한다.");
+        }
         this.mp = mp;
     }
 
@@ -35,6 +44,9 @@ public class Wizard {
     }
 
     public void setWand(Wand wand) {
+        if (wand == null) {
+            throw new IllegalArgumentException("마법사의 지팡이는 null일 수 없다.");
+        }
         this.wand = wand;
     }
 }
