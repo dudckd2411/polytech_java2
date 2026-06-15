db52e31a97e278b42cd74d056d6b814b54af4f84
Yungchang Cho <dudckd2411@gmail.com>
2026-06-15 16:56:43 +0900

    연습문제 1-1

---
+++ Game/src/main/java/com/survivalcoding/Wand.java
@@ -0,0 +1,23 @@
+package com.survivalcoding;
+
+public class Wand {
+    private String name;    //필드는 private으로 지정
+    private double power;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+}
+
---
+++ Game/src/main/java/com/survivalcoding/Wizard.java
@@ -0,0 +1,43 @@
+package com.survivalcoding;
+
+public class Wizard {
+    private int hp;
+    private int mp;
+    private String name;
+    private Wand wand;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getMp() {
+        return mp;
+    }
+
+    public void setMp(int mp) {
+        this.mp = mp;
+    }
+
+    public Wand getWand() {
+        return wand;
+    }
+
+    public void setWand(Wand wand) {
+        this.wand = wand;
+    }
+}
+
+
+
