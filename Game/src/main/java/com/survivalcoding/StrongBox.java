e4d053feab2eccbb1738a7616a1753b3b616a50f
Young Chang's <dudckd2411@gmail.com>
2026-06-30 00:55:02 +0900

    0629 4-2

--- Game/src/main/java/com/survivalcoding/StrongBox.java
+++ Game/src/main/java/com/survivalcoding/StrongBox.java
@@ -2,13 +2,47 @@ package com.survivalcoding;
 
 public class StrongBox<T> {
 
-    private T item;
+    public enum KeyType {
+        PADLOCK, // 자물쇠 (1024회)
+        BUTTON, // 버튼 (10000회)
+        DIAL, // 다이얼 (30000회)
+        FINGER // 지문 (1000000회)
+    }
 
-    public void put(T item) {
-        this.item = item;
+    private T item;
+    private KeyType keyType;
+    private long count;
+
+    public StrongBox(KeyType keyType) {
+        this.keyType = keyType;
+        this.count = 0;
     }
 
     public T get() {
+        this.count++;
+        long recount = 0;
+
+        switch (this.keyType) {
+            case PADLOCK:
+                recount = 1024;
+                break;
+            case BUTTON:
+                recount = 10000;
+                break;
+            case DIAL:
+                recount = 30000;
+                break;
+            case FINGER:
+                recount = 1000000;
+                break;
+        }
+        if (this.count < recount) {
+            return null;
+        }
         return this.item;
+    }
+
+    public void put(T item) {
+        this.item = item;
     }
 }
